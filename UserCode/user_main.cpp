2df9e336c4343f2feb116d17ac61d677338c4c6b
TITH <1023515576@qq.com>
2022-10-12 21:46:34 +0800

    标准流输出到USB

--- UserCode/user_main.cpp
+++ UserCode/user_main.cpp
@@ -4,34 +4,19 @@
 #include "usb_device.h"
 #include "usbd_cdc_if.h"
 #include "string.h"
+#include <iostream>
 
-uint8_t hello_str[] = "Hello USB!\n";
-
-/*USB 重新枚举函数*/
-void USB_Reset(void)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    GPIO_InitStruct.Pin = GPIO_PIN_12;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
-    HAL_Delay(100);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
-}
+using namespace std;
 
 void StartDefaultTask(void const *argument)
 {
     (void)argument;
-
-    USB_Reset();
+    
     MX_USB_DEVICE_Init();
 
     for (;;) {
         HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-        CDC_Transmit_FS(hello_str, strlen((const char *)hello_str));
+        cout << "Hello USB cout" << endl;
         osDelay(200);
     }
 }
